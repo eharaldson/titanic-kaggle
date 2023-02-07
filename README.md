84632bc006e29dd6a071006ebd3d4c29277431fa
Erik Haraldson <erikharaldson@MacBook-Pro.local>
2023-02-07 18:24:02 +0000

    adds logistic regression

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # titanic-kaggle
+
+Taking on the titanic kaggle competition. I will start by exploring the dataset, cleaning it, selecting features, and finally training and evalutaing different models.
+
+I will start with Logistic Regression to get a baseline.
--- modelling.py
+++ modelling.py
@@ -9,24 +9,36 @@ def eda(df):
     print()
     print(df.describe())
 
+def generate_clean_X_y(df):
+
+    df = df.fillna(df.mean(numeric_only=True))
+    le = preprocessing.LabelEncoder()
+    df['Sex'] = le.fit_transform(df['Sex'].to_list())
+    df = df.drop(['Cabin'], axis=1)
+    df = df.dropna(axis=0)
+    df['Embarked'] = le.fit_transform(df['Embarked'])
+
+    X = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Fare', 'Embarked']]
+    y = df['Survived']
+    return X, y
+
+def logistic_regression_eval(X_train, X_test, y_train, y_test):
+    logistic_model = linear_model.LogisticRegression()
+    param_grid = {'penalty': ['l1', 'l2'],
+                  'C': [0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 3.0, 5.0, 10.0]}
+    gs = model_selection.GridSearchCV(estimator=logistic_model, param_grid=param_grid, n_jobs=-1)
+    gs.fit(X_train, y_train)
+
+    return {'score': gs.best_score_,
+            'model': gs.best_estimator_,
+            'params': gs.best_params_}
+
 if __name__ == "__main__":
     
     df = pd.read_csv('train.csv')
 
-    eda(df)
+    X, y = generate_clean_X_y(df)
 
-    # le = preprocessing.LabelEncoder()
-    # df['Sex'] = le.fit_transform(df['Sex'])
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-    # print(df.head())
-
-    # X = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Fare', 'Embarked']]
-    # y = df['Survived']
-
-    # X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
-    # logistic_model = linear_model.LogisticRegression()
-
-    # logistic_model.fit(X_train, y_train)
-
-    # print(logistic_model.score(X_test, y_test))
+    print(logistic_regression_eval(X_train, X_test, y_train, y_test))
